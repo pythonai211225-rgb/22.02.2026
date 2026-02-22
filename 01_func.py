c5b5eab20cd36fd65212ba5fc005bcda09dce527
fullstack290623 <fullstack290623@gmail.com>
2026-02-22 18:32:33 +0200

    upload

---
+++ 01_func.py
@@ -0,0 +1,107 @@
+
+# do not repeat code
+# code imported from other projects/users
+# code from python input
+
+# FUNCTION <NAME>
+#   1...
+#   2...
+
+# <NAME>
+
+# <NAME>
+
+# lucky_number = get_random_numer()
+# max_guess = 10
+# current_guess = 0
+#
+# while True:
+#     user_number = get_number_from_user()
+#     current_guess += 1
+#     if user_number == lucky_number:
+#         print('BINGO!!')
+#         break
+#     if more_than_max():
+#         print("too many attempts")
+#         break
+#     if user_number > lucky_number:
+#         print('guess lower')
+#     if user_number < lucky_number:
+#         print('guess higher')
+
+# def ___name____():
+#   code
+# <----
+
+# def delete_drive_c():
+#     y = 1
+#
+# def greeting():
+#     x = 1
+#     delete_drive_c()
+#     print('hi')
+#
+#
+# print(greeting)
+# print(type(greeting))
+#
+# import random
+# x = random.randint(1, 100)
+#
+# greeting()
+# print('bye')
+
+# generic
+
+def greeting():
+    print('hello danny')
+
+def greeting2():
+    print('hello tomer')
+
+greeting2()
+
+print('hi')  # parameter
+
+def add(a = 1, b = 2):
+    result = a + b
+    print(result)
+
+def add2(a, b):
+    result = a + b
+    print(result)
+
+add(a = 4, b = 5)
+add(4, 5)
+add()
+
+add2()
+
+# def divide (a, b) --> check if b == 0 : print('cannot divide by zero') otherwise print (a/b)
+#   no default values
+#   input a, b before calling the function
+
+# create function even_odd which gets a number and if even print even if odd - odd
+#   even = zugi, odd - e-zugi
+#   input number from the user, call the function even_odd
+#   default value = 0
+
+def foo(list_ages, new_age):
+    pass
+
+foo([20, 30, 31, 36, 38], 42)
+
+# these are the ages of the group members: i.e. [20, 30, 31, 36, 38]
+# write a function that get the group ages as parameter
+#   and an age of a new member
+#   print if the new member is the oldest, youngest, or between
+#      [20, 30, 31, 36, 38]  42 --> oldest
+#      [20, 30, 31, 36, 38]  12 --> youngest
+#      [20, 30, 31, 36, 38]  32 --> between
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+# if elif else
+# while  ___, while True + break, for + break/ continue
+# match case : if else
+# direction = input()
+# match direction:
+#         case "up":print()
+#         case "down":print()
+#         case _:pass
+# int float str
+# str[::] -- mutable immutable
+# name = 'danny'
+# name = name + '!'
+# all any
+# list(str) <--> "".join(list)
+# list[::]
+# set
+# function
+# tuple
+# dict
+# programming
